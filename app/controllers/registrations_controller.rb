49c1f69b01217a8469c48f63a22934d66b5576fd
Chirag Aggarwal <me@chi6rag.net>
2016-01-07 10:55:19 +0530

    Refactoring Registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     user = User.find_by_email(params['user']['email'])
-    if (!user.nil? && user.valid?)
+    if (user_not_nil(user) && user.valid?)
       user.send_confirmation_instructions
     end
-    render nothing: true
+    redirect_to root_path
+  end
+
+  private
+  def user_not_nil(user)
+    !user.nil?
   end
 end
