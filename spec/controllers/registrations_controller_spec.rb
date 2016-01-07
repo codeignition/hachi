0febda2eeb646ff81a3e57d8b124933e494104f0
Chirag Aggarwal <me@chi6rag.net>
2016-01-07 12:18:36 +0530

    Refactoring Registrations

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -10,31 +10,35 @@ RSpec.describe RegistrationsController, :type => :controller do
     expect(response).to render_template :new
   end
 
-  context 'when tried to register a user' do
-    it 'a confirmation email is queued if he is already present' do
-      valid_user = User.create(email: 'test_1@example.com')
-      expect {
-        post :create, user: {email: valid_user.email}
-      }.to change { ActionMailer::Base.deliveries.count }.by(1)
+  describe 'when tried to register' do
+    context 'a user who is present in the database' do
+      it 'a confirmation email is queued' do
+        valid_user = User.create(email: 'test_1@example.com')
+        expect {
+          post :create, user: {email: valid_user.email}
+        }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
+
+      it 'is registered' do
+        valid_user = create(:valid_user)
+        post :create, user: attributes_for(:valid_user)
+        valid_user.reload
+        expect(valid_user).to be_registered
+      end
     end
 
-    it 'is registered if he is present' do
-      valid_user = create(:valid_user)
-      post :create, user: attributes_for(:valid_user)
-      valid_user.reload
-      expect(valid_user).to be_registered
-    end
+    describe 'a user who is not present in the database' do
+      it 'no confirmation email is queued' do
+        expect {
+          post :create, user: {email: 'unsaved_user-email@example.com'}
+        }.to_not change { ActionMailer::Base.deliveries.count }
+      end
 
-    it 'no confirmation email is queued if he is not present' do
-      expect {
-        post :create, user: {email: 'unsaved_user-email@example.com'}
-      }.to_not change { ActionMailer::Base.deliveries.count }
-    end
-
-    it 'is not registered if he is not present' do
-      user = build(:valid_user)
-      post :create, user: attributes_for(:valid_user)
-      expect(user).to_not be_registered
+      it 'is not registered' do
+        user = build(:valid_user)
+        post :create, user: attributes_for(:valid_user)
+        expect(user).to_not be_registered
+      end
     end
   end
 end
