ae85ba2eba39ca339a4c52b42abafa68822b7dfc
Chirag Aggarwal <me@chi6rag.net>
2016-01-18 14:43:24 +0530

    Chef: Downloading Mod Auth OpenIDC

--- chef/cookbooks/hachi_resource_server/recipes/default.rb
+++ chef/cookbooks/hachi_resource_server/recipes/default.rb
@@ -1,1 +1,8 @@
 package 'apache2'
+
+remote_file '/home/vagrant/libapache2-mod-auth-openidc_1.8.7-1ubuntu1.trusty.1_amd64.deb' do
+  source 'https://github.com/pingidentity/mod_auth_openidc/releases/download/v1.8.7/libapache2-mod-auth-openidc_1.8.7-1ubuntu1.trusty.1_amd64.deb'
+  owner 'vagrant'
+  group 'vagrant'
+  action :create
+end
--- chef/cookbooks/hachi_resource_server/test/integration/default/serverspec/default_spec.rb
+++ chef/cookbooks/hachi_resource_server/test/integration/default/serverspec/default_spec.rb
@@ -4,4 +4,9 @@ describe 'hachi_resource_server::default' do
   it 'should have apache 2' do
     expect(package('apache2')).to be_installed
   end
+
+  it 'should download mod auth openidc' do
+    mod_auth_openidc_file_name = 'libapache2-mod-auth-openidc_1.8.7-1ubuntu1.trusty.1_amd64.deb'
+    expect(File.exists? "/home/vagrant/#{mod_auth_openidc_file_name}").to eq(true)
+  end
 end
