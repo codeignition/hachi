9d0762c3b80c0de4603c2e1cbf52f7a4245f24ac
Chirag Aggarwal <me@chi6rag.net>
2016-01-18 15:27:32 +0530

    Chef: Installing libhiredis0.10, libcurl3 and libjansson4

--- chef/cookbooks/hachi_resource_server/recipes/default.rb
+++ chef/cookbooks/hachi_resource_server/recipes/default.rb
@@ -1,4 +1,7 @@
 package 'apache2'
+package 'libhiredis0.10'
+package 'libcurl3'
+package 'libjansson4'
 
 remote_file '/home/vagrant/libapache2-mod-auth-openidc_1.8.7-1ubuntu1.trusty.1_amd64.deb' do
   source 'https://github.com/pingidentity/mod_auth_openidc/releases/download/v1.8.7/libapache2-mod-auth-openidc_1.8.7-1ubuntu1.trusty.1_amd64.deb'
--- chef/cookbooks/hachi_resource_server/test/integration/default/serverspec/default_spec.rb
+++ chef/cookbooks/hachi_resource_server/test/integration/default/serverspec/default_spec.rb
@@ -5,6 +5,18 @@ describe 'hachi_resource_server::default' do
     expect(package('apache2')).to be_installed
   end
 
+  it 'should have libhiredis0.10 installed' do
+    expect(package('libhiredis0.10')).to be_installed
+  end
+
+  it 'should have libjansson4 installed' do
+    expect(package('libjansson4')).to be_installed
+  end
+
+  it 'should have libcurl3 installed' do
+    expect(package('libcurl3')).to be_installed
+  end
+
   it 'should download mod auth openidc' do
     mod_auth_openidc_file_name = 'libapache2-mod-auth-openidc_1.8.7-1ubuntu1.trusty.1_amd64.deb'
     expect(File.exists? "/home/vagrant/#{mod_auth_openidc_file_name}").to eq(true)
