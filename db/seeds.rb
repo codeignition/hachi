958b64eaad3880d0544abb6c3a562ce9abc44ba2
Chirag Aggarwal <me@chi6rag.net>
2016-01-19 18:31:57 +0530

    Adding sSeed OAuth Provider Application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Doorkeeper::Application.create(
+  name: 'SeedTestApp',
+  uid: 'seed_test_app',
+  secret: 'seed_test_app',
+  redirect_uri: 'urn:ietf:wg:oauth:2.0:oob'
+)
