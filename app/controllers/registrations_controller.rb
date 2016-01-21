5f1e447a12d626c3a9049988a467ec0da48eba72
Chirag Aggarwal <me@chi6rag.net>
2016-01-21 06:22:59 +0530

    Refactoring Registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,25 +2,25 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     ldap_users = LdapUsers.new
     user = ldap_users.find_by_common_name(common_name)
-    if (user_found?(user) && !user.registered?)
+    if (found?(user) && !user.registered? && user.valid?)
       user.save!
       user.send_confirmation_instructions
       user.register!
-      flash[:notice] = 'A confirmation email has been sent to you'
+      flash[:notice] = confirmation_notice
     end
     redirect_to root_path
   end
 
-  private
-  def user_not_nil(user)
-    !user.nil?
+  def confirmation_notice
+    'A confirmation email has been sent to you'
   end
 
+  private
   def common_name
     params['user']['email'].split('@').first
   end
 
-  def user_found?(user)
-    user_not_nil(user) && user.valid?
+  def found?(user)
+    !user.nil?
   end
 end
