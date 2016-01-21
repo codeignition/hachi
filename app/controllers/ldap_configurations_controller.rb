6bac8b03fabd7455d3d04da0c0af4df0c2e86638
Chirag Aggarwal <me@chi6rag.net>
2016-01-21 11:42:37 +0530

    Creation of LDAP Configuration with valid params redirects to root url

--- app/controllers/ldap_configurations_controller.rb
+++ app/controllers/ldap_configurations_controller.rb
@@ -2,4 +2,8 @@ class LdapConfigurationsController < ActionController::Base
   def new
     @ldap_configuration = LdapConfiguration.new
   end
+
+  def create
+    redirect_to root_path
+  end
 end
--- spec/controllers/ldap_configurations_controller_spec.rb
+++ spec/controllers/ldap_configurations_controller_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe LdapConfigurationsController, :type => :controller do
     get :new
     expect(response).to render_template :new
   end
+
+  describe 'creation of ldap configuration' do
+    it 'with valid params redirects to root path' do
+      post :create, ldap_configuration: valid_ldap_configuration
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  private
+  def valid_ldap_configuration
+    {
+      'host' => '192.168.33.101',
+      'port' => '389',
+      'dn' => 'example',
+      'search_base' => 'example_search_base',
+      'email' => 'test@example.com',
+      'ssh_public_key' => '123456789ABCDEFGHI',
+      'hachi_admin_usernames' => 'test_one,test_two,test_3',
+      'ldap_admin_username' => 'test_admin',
+      'ldap_admin_password' => '!Abcd1234'
+    }
+  end
 end
